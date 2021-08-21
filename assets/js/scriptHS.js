84614c664e094e46f3c8a71cfdb117cff68270a0
Matthew Pandolfo <matt@pandolfo.com>
2021-08-21 18:05:11 -0400

    displays all high scores in array

--- assets/js/scriptHS.js
+++ assets/js/scriptHS.js
@@ -1,17 +1,28 @@
 var scoreList = document.querySelector('#scoreList');
+var clearHS = document.querySelector('#clear-high-score')
 
 function highscores() {
   var savedHS = localStorage.getItem('allHSEntries');
   if(!savedHS) {
+    scoreList.textContent = "No high scores."
     return false
   }
 
   savedHS = JSON.parse(savedHS)
 
   for (let i = 0; i < savedHS.length; i++) {
-    console.log(savedHS[i]);
-    document.write(savedHS[i].inital + "<br>")
+    var scoreListLi = document.createElement('li');
+    scoreListLi.className = "highScorelist";
+    scoreListLi.textContent = (savedHS[i].inital + ' - ' + savedHS[i].score);
+    scoreList.appendChild(scoreListLi);
   }
 }
+
+clearHS.addEventListener('click', function(){
+  // clear all high scores
+  localStorage.removeItem('allHSEntries');
+  // refresh page
+  location.reload();
+})
 
 highscores();
